0e88d90e3b0f04226475dec5ca13243883fda32e
Smith <carliesmith@amrock.com>
2021-12-08 11:16:38 -0500

    almost finished but loop issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,16 @@
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
+var playerHealth = 10;
 var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
 
 
 var fight = function(enemyName) {
@@ -61,22 +66,39 @@ var fight = function(enemyName) {
   }
   }
 };
-for(var i = 0; i < enemyNames.length; i++) {
+var startGame = function() {
+  //reset player stats
+  playerHealth = 10;
+  playerAttack = 10;
+  playerMoney = 10;
+};
+var endGame = function () {
+  //if player is alive, player wins message
+  if (playerHealth > 0 ) {
+    window.alert("Congrats, you survived the game!  You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle");
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+      startGame();
+    }
+    else {
+      window.alert("Thank you for playing Robot Gladiators! Come back again!");
+};
+ for(var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
     window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName)
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
+  }
   else {
     window.alert("You have lost your robot in battle!  Boo!  Game over!");
     break;
   }
-  }
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
+  endGame();
 }
-//fight(enemyRobot)
+//start game when the page loads
+
